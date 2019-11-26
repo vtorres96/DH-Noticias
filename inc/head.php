699e6c86dc0601420dd99aa717b0208d905fb955
vtorres96 <victorcdc96@gmail.com>
2019-11-26 17:17:12 -0300

    optmized include files

--- inc/head.php
+++ inc/head.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="shortcut icon" type="imagem/x-icon" href="assets/img/logo-nahora-oficial.png">
-  <title>Portal DH</title>
+  <title>DH Notícias</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/estilo.css">
--- inc/header.php
+++ inc/header.php
@@ -1,61 +1,77 @@
 <header>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
-        <img src="assets/img/logo.png" width="100" height="40" class="d-inline-block mr-2" alt="Portal Na Hora">
+        <img src="assets/img/logo.png" width="120" height="40" class="d-inline-block mr-2" alt="Portal Na Hora">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-      <ul class="navbar-nav mr-auto">
         <?php if($active == "admin"): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="indexLogados.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="cadastroRedator.php">Cadastro de Redator</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="listandoRedatores.php">Listar Redatores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="listandoNoticias.php">Listar Notícias</a>
-          </li>
-        <?php elseif($active == "redator"): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="indexLogados.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="cadastroNoticia.php">Cadastrar Notícia</a>
-          </li>
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-            <a class="nav-link" href="listandoNoticias.php">Listar Notícias</a>
-          </li>
+              <a class="nav-link text-white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="cadastroRedator.php">Cadastro de Redator</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="listandoRedatores.php">Listar Redatores</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="listandoNoticias.php">Listar Notícias</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link name-user text-white mr-2">Olá, <?= $_SESSION["nome"] ?></a>
+            </li>
+            <a href="utils/auth/logout.php">
+              <button class="btn btn-outline-warning">Sair</button>
+            </a>
+          </ul>
+        <?php elseif($active == "redator"): ?>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="cadastroNoticia.php">Cadastrar Notícia</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-link text-white" href="listandoNoticias.php">Listar Notícias</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link name-user text-white mr-2">Olá, <?= $_SESSION["nome"] ?></a>
+            </li>
+            <a href="utils/auth/logout.php">
+              <button class="btn btn-outline-warning">Sair</button>
+            </a>
+          </ul>
         <?php elseif($active == "comum"): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#noticias">Notícias</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contato">Contato</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#localizacao">Localização</a>
-          </li>
-        <?php endif;  ?>
-      </ul>
-      <?php if($active == "admin" || $active == "redator"): ?>
-        <?php session_start();  ?>
-        <div class="form-inline my-2 my-lg-0">
-          <div class="name-user mr-3">Olá, <?= $_SESSION["nome"] ?></div>
-          <a href="../utils/logout.php">
-            <button class="btn btn-outline-info my-2 my-sm-0">Sair</button>
-          </a>
-        </div>
-      <?php endif  ?> 
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#noticias">Notícias</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#contato">Contato</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#localizacao">Localização</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="login.php">Minha Conta</a>
+            </li>
+          </ul>
+        <?php endif; ?>
     </div>
   </nav>
 </header>
