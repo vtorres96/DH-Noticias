b1ecfcfff1aadf12a4c9a5ca67190b9c9b928a63
vtorres96 <victorcdc96@gmail.com>
2019-11-26 22:31:48 -0300

    added return message to validation

--- utils/auth/validacao.php
+++ utils/auth/validacao.php
@@ -1,6 +1,8 @@
 <?php
   require_once("../../config/conn.php");
 
+  require_once("../returnMessage.php");
+
   $email = $_REQUEST["email"];
   $senha = $_REQUEST["senha"];
 
@@ -16,27 +18,24 @@
 
   // Se não encontrar usuário retonarmos erro
   if(!$usuario){
-    $erro = true;
+    echo retornaMensagem("Usuário não encontrado");
+    return;
   } else {
       // Se a senha não conferir com a do banco de dados
       $senhaValida = password_verify($senha, $usuario["senha"]);
       if(!$senhaValida){
-        $erro = true;
+        echo retornaMensagem("Usuário ou senha inválidos");
+        return;
       } else {
         // Se encontrar usuário e a senha estiver correta criamos sessão
         // e redirecionamos o usuário
         session_start();
-
         $_SESSION["logado"] = true;
         $_SESSION["nome"] = $usuario["nome"];
         $_SESSION["nivel_acesso"] = $usuario["nivel_acesso"];
 
-        header("Location: ../../index.php");
+        echo retornaMensagem("Redirecionando...", true);
       }
-  }
-
-  if(isset($erro) && $erro === true){
-    echo "Usuário ou senha inválidos";
   }
 
 ?>
---
+++ utils/returnMessage.php
@@ -0,0 +1,11 @@
+<?php
+    // Função que retorno um JSON com a propriedade sucesso e mensagem
+    function retornaMensagem($mensagem, $sucesso = false){
+        // Criando vetor com a propriedades
+        $retorno = array();
+        $retorno['sucesso'] = $sucesso;
+        $retorno['mensagem'] = $mensagem;
+        // Convertendo para JSON e retornando
+        return json_encode($retorno);
+    }
+?>
