b1c8c03a04c42934c34b4b72cacafe95c73f2ad2
vtorres96 <victorcdc96@gmail.com>
2019-11-27 14:31:36 -0300

    added generic scripts in scripts.php

---
+++ inc/scripts.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script src="http://malsup.github.io/min/jquery.form.min.js"></script>
