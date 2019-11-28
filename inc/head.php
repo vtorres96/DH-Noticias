5ec78da958b3aec5e35138e1e01f33af772ec9a5
vtorres96 <victorcdc96@gmail.com>
2019-11-28 17:55:42 -0300

    changed title page

--- inc/head.php
+++ inc/head.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="shortcut icon" type="imagem/x-icon" href="assets/img/logo-nahora-oficial.png">
-  <title>DH Notícias</title>
+  <title>Só Notícia Boa</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css"> 
